62e4556aa71d7da197dee1fc77e70b2496f7e0dc
Razvan150 <102427978+Razvan150@users.noreply.github.com>
2026-09-20 12:11:21 +0300

    Create Driver.h

---
+++ Driver.h
@@ -0,0 +1,22 @@
+#ifndef Driver_h
+#define Driver_h
+class Driver : public User {
+private:
+    std::string masina;
+    bool disponibil;
+
+public:
+    Driver(std::string nume, int age, float rating, int balance, std::string masina, bool disponibil);
+
+    ~Driver();
+
+    void setDisponibil(bool val);
+    void setMasina(std::string m);
+
+    std::string getMasina() const;
+    bool getDisponibil() const;
+
+    void finishRide();
+    void afisare() const override;
+};
+#endif
